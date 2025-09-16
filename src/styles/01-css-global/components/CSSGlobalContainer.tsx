cafe32b410768187eee6269701479c2bd5c6ad17
devgabrielnascimento <devgabrielnascimento@gmail.com>
2025-09-15 23:58:09 -0300

    :lipstick: style: rename global file in CSS Global

--- src/styles/01-css-global/components/CSSGlobalContainer.tsx
+++ src/styles/01-css-global/components/CSSGlobalContainer.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "../../../hooks/useTheme";
 import { Navbar } from "./Navbar";
-import "../../../styles/01-css-global/globals.css";
+import "../../../styles/01-css-global/global.css";
 import { MainContent } from "./MainContent";
 
 export function CSSGlobalContainer() {
